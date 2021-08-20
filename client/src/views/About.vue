93f0b56d556a65ad7f4c9d56d1f6912eb3a4f9be
heanphearom <heanphearom7@gmail.com>
2021-08-20 15:21:35 +0700

    Updat form local us fix pass.

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi excepturi reprehenderit sed reiciendis consequatur sapiente modi? Et, odit mollitia assumenda incidunt quidem molestias, tempora officiis error atque quisquam eos non!</p>
   </div>
 </template>
